b5132654664cafc62f042c1130bbc61181dcca75
panghaibin <panghaibin@shu.edu.cn>
2021-09-28 22:29:28 +0800

    feat: 消息发送实现

--- main.py
+++ main.py
@@ -1,36 +1,114 @@
 # -*- coding: utf8 -*-
+import json
+import requests
 from setting import Setting
 from config.default import DefaultHealthReport
 from config.nnnu import NNNUHealthReport
 from config.test import TestReport
 
 
-if __name__ == '__main__':
-    default_user = Setting().get_users(config='default')
-    nnnu_user = Setting().get_users(config='nnnu')
-    test_user = Setting().get_users(config='test')
-    results = []
-    for user in default_user:
-        try:
-            r = DefaultHealthReport(user['username'], user['password'])
-            t = r.report()
-            results.append(t)
-        except Exception as e:
-            results.append(e)
-    for user in nnnu_user:
-        try:
-            r = NNNUHealthReport(user['username'], user['password'])
-            t = r.report()
-            results.append(t)
-        except Exception as e:
-            results.append(e)
-    for user in test_user:
-        try:
-            r = TestReport(user['username'], user['password'])
-            t = r.report()
-            results.append(t)
-        except Exception as e:
-            results.append(e)
+class MainHandle(object):
+    """
+    主函数
+    """
 
-    # TODO: 实现消息推送
-    print(results)
+    def __init__(self):
+        setting = Setting()
+        self._default_user = setting.get_users(config='default')
+        self._nnnu_user = setting.get_users(config='nnnu')
+        self._test_user = setting.get_users(config='test')
+        self.results = []
+
+        for user in self._default_user:
+            try:
+                r = DefaultHealthReport(user['username'], user['password'])
+                t = r.report()
+                self.results.append(t)
+            except Exception as e:
+                self.results.append(str(e))
+        for user in self._nnnu_user:
+            try:
+                r = NNNUHealthReport(user['username'], user['password'])
+                t = r.report()
+                self.results.append(t)
+            except Exception as e:
+                self.results.append(str(e))
+        for user in self._test_user:
+            try:
+                r = TestReport(user['username'], user['password'])
+                t = r.report()
+                self.results.append(t)
+            except Exception as e:
+                self.results.append(str(e))
+
+
+class SendMsg(object):
+    """
+    消息发送函数
+    """
+
+    def __init__(self, results):
+        if not results:
+            raise Exception('消息内容不能为空')
+        self.results = results
+        self._api_types = ['未设置', '方糖气球', '推送加']
+        setting = Setting()
+        self.api_type = setting.api_type
+        self.api_key = setting.api_key
+        if self.api_type not in range(1, len(self._api_types)) or self.api_key == '':
+            raise Exception('未配置消息发送类型及 key')
+        self.api_type_name = self._api_types[self.api_type]
+        self.send_result = {}
+        self.send_result_bool = None
+        self.desp = ''
+        for i in self.results:
+            self.desp += i + "\n\n"
+
+    def server_chan(self):
+        url = "http://sctapi.ftqq.com/%s.send" % self.api_key
+        data = {
+            'text': self.results[0],
+            'desp': self.desp
+        }
+        text = requests.post(url, data=data).text
+        result = json.loads(text)
+        self.send_result = result
+        if result['data']['error'] == 'SUCCESS':
+            return True
+        else:
+            return False
+
+    def push_plus(self):
+        url = "http://www.pushplus.plus/send"
+        data = {
+            'token': self.api_key,
+            'title': self.results[0],
+            'content': self.desp,
+            'template': 'markdown'
+        }
+        headers = {'Content-Type': 'application/json'}
+        body = json.dumps(data).encode(encoding='utf-8')
+        text = requests.post(url, data=body, headers=headers).text
+        result = json.loads(text)
+        self.send_result = result
+        if result['code'] == 200:
+            return True
+        else:
+            return False
+
+    def send_msg(self):
+        result_bool = False
+        if self.api_type == 1:
+            result_bool = self.server_chan()
+        elif self.api_type == 2:
+            result_bool = self.push_plus()
+        self.send_result_bool = result_bool
+        return self.send_result_bool
+
+
+if __name__ == '__main__':
+    main = MainHandle()
+    print(main.results)
+    send = SendMsg(main.results)
+    send.send_msg()
+    print(send.send_result)
--- setting.py
+++ setting.py
@@ -7,17 +7,24 @@ class Setting(object):
     def __init__(self):
         self._abs_path = os.path.split(os.path.realpath(__file__))[0]
         self.setting_path = self._abs_path + "/setting.yaml"
+        self._check_setting()
         self.users = {}
         self.user_list = []
+        self.api_type = self.setting.get('send_api', 0)
+        self.api_key = self.setting.get('send_key', '')
 
-    def check_setting(self):
+    def _check_setting(self):
         if not os.path.exists(self.setting_path):
             print('未找到配置文件')
             print('请创建 setting.yaml 文件')
             print('格式参考 setting.bak.yaml')
             raise Exception('配置文件不存在')
         with open(self.setting_path, 'r', encoding='utf-8') as f:
-            users = yaml.load(f, Loader=yaml.FullLoader)['users']
+            setting = yaml.load(f, Loader=yaml.FullLoader)
+        self.setting = setting
+
+    def _check_users(self):
+        users = self.setting.get('users', [])
         for user in list(users.keys()):
             if 'username_here' in user:
                 users.pop(user)
@@ -25,7 +32,7 @@ class Setting(object):
         return self.users
 
     def get_users(self, config=None):
-        self.check_setting()
+        self._check_users()
         users = self.users
         user_list = []
         for username in list(users.keys()):
