b3b40f1e11de2a7a5e91335a5c34420e4efbcef8
jxz2333 <73346681+jxz2333@users.noreply.github.com>
2022-10-13 10:54:46 +0800

    适配cwxu表单 (#23)
    * 适配无锡学院新表单
    
    * Update cwxu.py
    
    适配无锡学院新表单

--- config/cwxu.py
+++ config/cwxu.py
@@ -11,15 +11,13 @@ class CWXUHealthReport(_Report):
         self._form_id = '3449'
         self._enc = '627c625902a1fd27de56172186a3f903'
         self._reporter_name = 'CWXU健康打卡'
-
-        self._day_id = -1
-        self._report_time_id = -1
+        self._day_id = [-1]
+        self._report_time_id = [24]
         # 该表单有多个体温
         self._temperature_ids = [1, 16]
         self._options_ids = [13, 3, 7, 5, 11, 6, 8, 12]
         self._hasAuthority_ids = [14, 15]
         self._isShow_ids = []
-
     def _clean_form_data(self):
         form_data = self._last_form_data
         for f in form_data:
@@ -32,10 +30,10 @@ class CWXUHealthReport(_Report):
                     raise Exception(self._result)
                 else:
                     f['fields'][0]['values'][0]['val'] = today
-            elif f['id'] == self._report_time_id:
+            elif f['id'] in self._report_time_id:
                 # 打卡时间
                 today = self._t.today
-                report_time = self._t.report_time
+                report_time = self._t.today
                 if f['fields'][0]['values'][0]['val'].startswith(today):
                     # 同上
                     self._result = '%s今日%s已填报过%s' % (self._username_masked, today, self._reporter_name)
@@ -57,6 +55,5 @@ class CWXUHealthReport(_Report):
             elif f['id'] in self._isShow_ids:
                 # 内部使用的id
                 f['isShow'] = False
-
         self._today_form_data = form_data
         return form_data
